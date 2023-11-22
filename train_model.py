8a9f6c065bef2eb9e720424d35dafc743e2fe865
Hamza Raza <hamzaraza77094@gmail.com>
2023-11-22 11:13:00 -0600

    Small change

--- train_model.py
+++ train_model.py
@@ -3,8 +3,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-import joblib
 from sklearn.metrics import accuracy_score
+import joblib
 
 
 # Prepare Dataset with provided data
